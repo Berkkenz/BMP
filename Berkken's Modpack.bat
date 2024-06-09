bc0951f2ac9bb6a32476fe0f9dba5d76b915cdc9
Berkkenz <berkkenbusiness@gmail.com>
2024-06-09 05:29:28 -0400

    Update Berkken's Modpack.bat

--- Berkken's Modpack.bat
+++ Berkken's Modpack.bat
@@ -85,10 +85,9 @@ if not exist %MCPATH% (
 
 if not exist "%AppData%\.minecraft\versions\1.16.5-forge-36.2.42\1.16.5-forge-36.2.42.jar" (
 	curl -L -o %temp%\forgeinstaller.jar "https://maven.minecraftforge.net/net/minecraftforge/forge/1.16.5-36.2.42/forge-1.16.5-36.2.42-installer.jar"
-	setlocal
-	set APPDATA=%MCPATH%
-	%JAVA8PATH% -jar %temp%\forgeinstaller.jar --installClient --target %MCPATH%
-	endlocal
+	pushd %MCPATH%
+	%JAVA8PATH% -jar %temp%\forgeinstaller.jar --installClient
+	popd
 	del %temp%\forgeinstaller.jar /Q /F
 	if not exist "%appdata%\.minecraft\versions\1.16.5-forge-36.2.42\1.16.5-forge-36.2.42.jar" (
 		echo Forge install has failed, Exiting...
