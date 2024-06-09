78de32033f4518e3de1e6b783893469fed7ecd27
Berkkenz <berkkenbusiness@gmail.com>
2024-06-09 04:02:19 -0400

    Update Berkken's Modpack.bat

--- Berkken's Modpack.bat
+++ Berkken's Modpack.bat
@@ -42,7 +42,7 @@ timeout 2
 :: JAVA 17
 if not exist %JAVA17PATH% (
 	echo Java 17 not installed, Attempting installation...
-	curl -L -o "%temp%\java17installer.exe" "https://download.oracle.com/java/17/archive/jdk-17.0.10_windows-x64_bin.exe"
+	curl -L -o %temp%\java17installer.exe "https://download.oracle.com/java/17/archive/jdk-17.0.10_windows-x64_bin.exe"
 	start /wait "%temp%\java17installer.exe"
 	del %temp%\java17installer.exe" /Q /F
 	if not exist %JAVA17PATH% (
@@ -58,7 +58,7 @@ if not exist %JAVA17PATH% (
 :: JAVA 8
 if not exist %JAVA8PATH% (
 	echo Java 8 not installed, Attempting installation...
-	curl -L -o "%appdata%\java8installer.exe" "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=249833_43d62d619be4e416215729597d70b8ac"
+	curl -L -o %appdata%\java8installer.exe "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=249833_43d62d619be4e416215729597d70b8ac"
 	start %temp%\java8installer.exe
 	del "%temp%\java8installer.exe" /Q /F
 	if not exist %JAVA8PATH% (
