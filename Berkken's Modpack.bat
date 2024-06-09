428d0f605b40607e071eb6a2ce6f0a7a77961cba
Berkkenz <berkkenbusiness@gmail.com>
2024-06-09 19:16:46 -0400

    Update Berkken's Modpack.bat

--- Berkken's Modpack.bat
+++ Berkken's Modpack.bat
@@ -23,6 +23,7 @@ if not exist %GITPATH% (
 	cls
 	echo Git has been installed, please restart the application!
 	echo Update completed... Please restart!
+	start /min wmplayer "%SystemRoot%\Media\Windows Notify.wav"
 	echo MsgBox "Git Install Successful. Please Restart the Application.", vbOKOnly, "UPDATE SUCCESSFUL!." > "%temp%\gittemp.vbs"
 	cscript //nologo "%temp%\gittemp.vbs"
 	del "%temp%\gittemp.vbs" /Q /F
@@ -47,6 +48,7 @@ git diff --quiet origin/main || (
 	echo Updates found! Updating...
 	git reset --hard origin/main
 	echo Update completed... Please restart!
+	start /min wmplayer "%SystemRoot%\Media\Windows Notify.wav"
 	echo MsgBox "Update Successful. Please Restart the Application.", vbOKOnly, "UPDATE SUCCESSFUL!." > "%temp%\hubtemp.vbs"
 	cscript //nologo "%temp%\hubtemp.vbs"
 	del "%temp%\hubtemp.vbs" /Q /F
@@ -129,6 +131,7 @@ xcopy "%~dp0\game\.minecraft\emotes" "%MCPATH%\emotes" /e /h /i /y
 
 setlocal
 set VBSCRIPT=%temp%\prompt.vbs
+start /min wmplayer "%SystemRoot%\Media\Windows Notify.wav"
 echo Set WshShell = WScript.CreateObject("WScript.Shell") > %VBSCRIPT%
 echo response = WshShell.Popup("Would you like to apply the recommended settings?            This will OVERWRITE your current settings.", 0, "Confirmation", 4 + 32) >> %VBSCRIPT%
 echo WScript.Quit response >> %VBSCRIPT%
@@ -145,6 +148,7 @@ if %response% == 6 (
 
 cls
 echo Update Completed! Enjoy your Mods!
+start /min wmplayer "%SystemRoot%\Media\Windows Notify.wav"
 echo MsgBox "Installation Finised. Enjoy your Mods!", vbOKOnly, "UPDATE SUCCESSFUL!." > "%temp%\fintemp.vbs"
 cscript //nologo "%temp%\fintemp.vbs"
 del "%temp%\fintemp.vbs" /Q /F
